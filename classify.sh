a7971b597c1b87469452729d741d252a921571a2
Dean Wyatte <dean.r.wyatte@gmail.com>
2019-05-19 13:11:39 -0600

    shut down image/volumes, enable batching

--- classify.sh
+++ classify.sh
@@ -2,4 +2,5 @@
 
 docker-compose up tensorflow-serving & \
 sleep 5
-export TF_SERVING_URL=localhost:8500 && python classification/run.py $@
+export TF_SERVING_URL=localhost:8500 && python classification/run.py $@ && \
+docker-compose down -v
